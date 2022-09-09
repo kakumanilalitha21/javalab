8c2b55dd74737a523ca964530f98567c26b36e39
kakumanilalitha21 <113081900+kakumanilalitha21@users.noreply.github.com>
2022-09-09 09:58:58 +0530

    Update InvoiceTest.java

--- InvoiceTest.java
+++ InvoiceTest.java
@@ -6,16 +6,16 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 Invoice in=new Invoice();
 System.out.print("Enter part number :");
-in.setPart_number(sc.nextLine());
+in.setnumber(sc.nextLine());
 System.out.print("Enter part description :");
-in.setPart_description(sc.nextLine());
+in.setdescription(sc.nextLine());
 System.out.print("Enter no. of items purchased :");
 in.setQuantity(sc.nextInt());
 System.out.print("Enter price per item :");
-in.setPrice_item(sc.nextDouble());
+in.setPriceperitem(sc.nextDouble());
 System.out.print(" Item Details \n");
-System.out.print("Part number :" + in.getPart_number());
-System.out.print("\nPart description :" + in.getPart_description());
+System.out.print("Part number :" + in.getnumber());
+System.out.print("\nPart description :" + in.getdescription());
 System.out.print("\nTotal Billing Amount :" + in.getInvoiceAmount());
 }
 }
