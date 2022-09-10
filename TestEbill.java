a59db335ed1aa2449081503a298ca93e03f3ecf0
KAKUMANI LALITHA <113081900+kakumanilalitha21@users.noreply.github.com>
2022-09-10 19:24:23 +0530

    Add files via upload

---
+++ TestEbill.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TestEbill
+{
+	public static void main(String[] args) 
+	{
+		ElectricityBill e=new ElectricityBill();
+		e.getDetails();
+		e.billamount();
+		e.showBillamount();
+	}
+}
