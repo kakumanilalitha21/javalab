801546cd9887ebb5cd84cdf58e7ecf0240b2babf
KAKUMANI LALITHA <113081900+kakumanilalitha21@users.noreply.github.com>
2022-09-11 18:13:48 +0530

    Update TestSavingsaccount.java

--- TestSavingsaccount.java
+++ TestSavingsaccount.java
@@ -2,16 +2,16 @@ class TestSavingsaccount
 {
 	public static void main(String args[]) 
 	{
-		SavingsAccount ob1=new SavingsAccount(2000);
-		SavingsAccount ob2=new SavingsAccount(3000);
+		SavingsAccount saver1=new SavingsAccount(2000);
+		SavingsAccount saver2=new SavingsAccount(3000);
 		ob1.modifyInterestrate(0.04);
-		ob1.MonthlyInterest();
-		ob2.modifyInterestrate(0.04);
-		ob2.calculateMonthlyInterest();
+		saver1.MonthlyInterest();
+		saver2.modifyInterestrate(0.04);
+		saver2.calculateMonthlyInterest();
 		System.out.println("after changing interest rate from 4% to 5%");
-		ob1.modifyInterestrate(0.05);
-		ob2.MonthlyInterest();
-		ob1.modifyInterestrate(0.05);
-		ob2.MonthlyInterest();
+		saver1.modifyInterestrate(0.05);
+		saver2.MonthlyInterest();
+		saver1.modifyInterestrate(0.05);
+		saver2.MonthlyInterest();
 	}
 }
