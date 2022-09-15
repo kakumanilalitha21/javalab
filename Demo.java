8cbf0ff64712c32555b3ce981b2a140d699ab8da
KAKUMANI LALITHA <113081900+kakumanilalitha21@users.noreply.github.com>
2022-09-15 09:10:04 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Demo{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("enter number:");
+        int num=sc.nextInt();
+}
+}
