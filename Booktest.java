165a685275808821989e162ad74db605ce1343ac
KAKUMANI LALITHA <113081900+kakumanilalitha21@users.noreply.github.com>
2022-10-25 18:56:05 +0530

    Delete Booktest.java

--- Booktest.java
+++
@@ -1,9 +0,0 @@
-public class Booktest {
-public static void main(String[] args) {
-Book arr[]=new Book[30];
-arr[1]=new Book("Database system concepts",11260,"Abraham silbetschatz","Mc Graw hill");
-arr[1].getBookInfo();
-arr[2]=new Book("Mobile Computing",12423,"vinay kumar singhal","K nath&sons");
-arr[2].getBookInfo();
-}
-}
