a6949b112cb4004a5565f003354fc2c210db37e7
KAKUMANI LALITHA <113081900+kakumanilalitha21@users.noreply.github.com>
2022-10-25 18:56:15 +0530

    Delete Demo.java

--- Demo.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Demo{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        String name=sc.nextLine();
-        System.out.println("enter number:");
-        int num=sc.nextInt();
-}
-}
